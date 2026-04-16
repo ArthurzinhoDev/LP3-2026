b82b226efbe8fdbd590273fc3d3d733df5292c56
ArthurzinhoDev <152618050+ArthurzinhoDev@users.noreply.github.com>
2026-04-16 14:18:28 +0000

    Criação da API

---
+++ calculadora/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:8000");
+var app = builder.Build();
+
+app.MapGet("/", () =>
+{
+    return new { mensagem = "API em execução ..." };
+});
+
+//um app.map para cada operação, ou seja, 4 app.map para as 4 operações
+
+
+app.MapGet("/calcula/soma/{valor1}/{valor2}", (int valor1, int valor2) =>
+{
+    return Results.Ok(new { mensagem = $"{valor1} + {valor2} = {valor1 + valor2}" });
+});
+
+app.MapGet("/calcula/subtracao/{valor1}/{valor2}", (int valor1, int valor2) =>
+    {  
+    return Results.Ok(new { mensagem = $"Execução do caso 2: {valor1} - {valor2} = {valor1 - valor2}" });     
+});
+
+app.MapGet("/calcula/multiplicacao/{valor1}/{valor2}", (int valor1, int valor2) =>
+{   
+    return Results.Ok(new { mensagem = $"Execução do caso 3: {valor1} * {valor2} = {valor1 * valor2}" });
+});
+
+app.MapGet("/calcula/divisao/{valor1}/{valor2}", (int valor1, int valor2) =>
+{
+            if (valor2 == 0)
+            {
+                return Results.BadRequest(new { erro = "Erro: Divisão por zero não é permitida." });
+            }
+            else{
+            return Results.Ok(new { mensagem = $"Execução do caso 4: {valor1} / {valor2} = {valor1 / valor2}" });
+            }
+            
+    }
+);
+
+app.Run();
