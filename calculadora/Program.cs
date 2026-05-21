4194c42888ceb053cbe70ebed9b0ee6a521a50e6
ArthurzinhoDev <152618050+ArthurzinhoDev@users.noreply.github.com>
2026-05-21 13:59:34 +0000

    api-vetores-criada

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+gitvar builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:8000");
 var app = builder.Build();
 
